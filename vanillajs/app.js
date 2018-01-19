df4d63a48a92489bb1006895d895e639c0482ed0
jkrayer <jameskrayer@yahoo.com>
2018-01-19 08:42:57 -0500

    Documentation and closure

--- vanillajs/app.js
+++ vanillajs/app.js
@@ -1,4 +1,9 @@
-var modFields = document.getElementsByClassName('mod');
+
+/**
+ * calculate and show final scores
+ * @param  {HTMLCollection} fields elements tha show the calculated value
+ * @return {Function}              actual calculation
+ */
 var calculate = (function(fields) {
   return function(raceMods) {
     raceMods.forEach(function(mod, ind) {
@@ -7,15 +12,24 @@ var calculate = (function(fields) {
   }
 }(document.getElementsByClassName('total')));
 
-function handleRaceChange() {
-  var raceMods = this.value.split(',');
 
-  raceMods.forEach(function(mod, ind) {
-    modFields[ind].innerText = mod;
-  });
+/**
+ * Handle Change event for the race select element
+ * @param  {HTMLCollection} modFields elements that show the modifier
+ * @return {Function}                 event handler
+ */
+var handleRaceChange = (function (modFields) {
+  return function() {
+    var raceMods = JSON.parse(this.value);
 
-  calculate(raceMods, null, null);
-}
+    raceMods.forEach(function(mod, ind) {
+      modFields[ind].innerText = mod;
+    });
+
+    calculate(raceMods, null, null);
+  }
+}(document.getElementsByClassName('mod')));
+
 
 // Attach events
 (function() {
